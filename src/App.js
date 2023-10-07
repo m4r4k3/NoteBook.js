0e5354b25cd8e04ec5dee0583407b6de7027e5b7
m4r4k <marwaneakchar@gmail.com>
2023-10-06 23:49:01 -0400

    learning react

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import First from './first';
+import './style.css';
+
+
+export default function App() {
+    return (<First />);
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDom from 'react-dom';
+import React from 'react';
+import App from './App';
+
+
+ReactDom.render(<App />, document.getElementById('root'));
