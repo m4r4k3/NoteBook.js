c2f5d22553de78dc9cf1f840aa15bccd1f8bd1ae
m4r4k <marwaneakchar@gmail.com>
2023-10-08 21:21:01 -0400

    adjusted the line height and stored notes in localstorage

--- src/components/text.js
+++ src/components/text.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
 export default function Text() {
+    React.useEffect(
+        () => {
+            document.querySelector('textarea').innerHTML = localStorage.getItem('note');
+            window.onunload = () => {
+                localStorage.setItem('note', document.querySelector('textarea').value);
+            };
+        }, []);
     return (<textarea
         className="textarea"
         rows="23"
         style={
-        { lineHeight: `${((window.innerHeight) / 24)}px`,
-            padding: `${Math.round(window.innerHeight / 41.2)}px  0 0 ${Math.round(window.innerWidth / 11) + 5}px `,
+        {
+            lineHeight: `${((window.innerHeight) / 24)}px`,
+            padding: `${(window.innerHeight / 24) - 26.125}px  0 0 ${Math.round(window.innerWidth / 11) + 5}px `,
         }
     } />);
 }
